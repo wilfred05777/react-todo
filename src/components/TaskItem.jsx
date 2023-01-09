43032d2896395fb9eac81c841c1e24a20292155d
Wilfred <wilfred05777@gmail.com>
2023-01-09 17:07:01 +0800

    0:45:56

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { useState } from "react";
+
+/// styles
+import styles from "./TaskItem.module.css";
+
+// library imports
+import { CheckIcon } from "@heroicons/react/24/outline";
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import { TrashIcon } from "@heroicons/react/24/outline";
 
 const TaskItem = ({ task }) => {
-  return <div>{task.name}</div>;
+  const [isChecked, setIsChecked] = useState(task.checked);
+
+  const handleCheckbox = (e) => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <li className={styles.task}>
+      <div className={styles["task-group"]}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          // checked={task.checked}
+          checked={isChecked}
+          onChange={handleCheckbox}
+          task={task.name}
+          id={task.id}
+        />
+        <label htmlFor={task.id} className={styles.label}>
+          {task.name}
+          <p className={styles.checkmark}>
+            <CheckIcon strokeWidth={2} width={24} height={24} />
+          </p>
+        </label>
+      </div>
+    </li>
+  );
 };
 
 export default TaskItem;
